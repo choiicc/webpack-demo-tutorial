a5c8a9831d89648bed891e0dd5ae517c48a029db
choi <choi@betbao.com>
2019-03-08 11:43:16 +0800

    ts

---
+++ demo13/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'typescript'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx','.ts','.js']
+    },
+    devtool: 'inline-source-map'
+}
